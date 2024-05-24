219544c7c06608df4b60aa70d85f16ff0c26e508
nazar <nazar.zrey@gmail.com>
2024-05-25 06:33:21 +0700

    commit zrey projek unpam 20240525  633

--- absen_ci3_backend/application/models/Absensi_model.php
+++ absen_ci3_backend/application/models/Absensi_model.php
@@ -22,7 +22,7 @@ class Absensi_model extends CI_Model {
         // $this->db->from('unpam_matkul');
         // $query = $this->db->get();
         // return $query->result_array();
-        $sql = "SELECT a.*,max(b.`updrec_date`) as sync FROM unpam_matkul a left join unpam_absen_log b
+        $sql = "SELECT a.*,date_format(max(b.`updrec_date`),'%d-%m-%y %H:%i') as sync FROM unpam_matkul a left join unpam_absen_log b
         on trim(a.`dosen`)=trim(b.`obj_dosen`) group by a.`dosen`;";
         $query = $this->db->query($sql);
         return $query->result_array();
--- absen_ci3_backend/application/views/rekap_absensi_view.php
+++ absen_ci3_backend/application/views/rekap_absensi_view.php
@@ -4,35 +4,50 @@
     <meta charset="UTF-8">
     <title>Rekap Absensi</title>
     <style>
+        body {
+            font-family: Verdana, sans-serif;
+            margin: 0;
+        }
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+        th, td {
+            padding: 5px 10px;
+            text-align: center;
+        }
+        thead {
+            background: #f1f1f1;
+            position: sticky;
+            table-layout: fixed;
+            top: 0;
+            z-index: 1;
+        }
+        tbody {
+            /* display: block; */
+            overflow-y: auto;
+            width: 100%;
+        }
         .empty {
             background-color: #e6fbff;
         }
-        table{border-collapse:collapse}body{font-family:verdana;margin:0}td{font-size:12px;padding:5px 2px;cursor:pointer}th{font-size:14px;padding:5px 0}.tdc{text-align:center}.tdb{background:#f1f1f1}
-        .kurang{background:#f3e29f}
-        .merah{background:#ffafa1}
-        .popup {
-            position: absolute; /* Make it absolute for positioning */
-            background-color: #fff; /* Set background color */
-            border: 1px solid #ccc; /* Add a border */
-            padding: 10px; /* Add padding for content */
-            display: none; /* Initially hide the popup */
-            font-size:14px;
-            width:160px
+        .kurang {
+            background: #f3e29f;
         }
-        .tdc:hover{background:tomato}
-        .ce{text-align:center}
-        .tdc,.tdb{
-            <?= $lweek <= 20 ? "width:50px" :"width:40px"; ?>;
-            height:20px !important;
+        .merah {
+            background: #ffafa1;
         }
-        .tr{text-align:right}
-        .pert{background:#b1ffcf}
-        .judul{padding:10px;font-size:20px}
-        .perj{background:#8ae2be;font-size:14px;font-weight:normal}
-        .sync{width: 70px;font-size:10px}
-        th{text-align:center;padding:10px}
-        td{text-align:center;padding:5px 10px}
-        tr td:nth-child(2){text-align:left}
+        .sync {
+            width: 70px;
+            font-size: 10px;
+        }
+        .perj {
+            background: #8ae2be;
+            font-size: 14px;
+            font-weight: normal;
+        }
+        .t1{width: 300px;}
+        .t2{width:100px}
     </style>
 </head>
 <body>
@@ -40,39 +55,43 @@
     <a href="<?= base_url('grup/'. (($week - 1)>=8?($week - 1):"#")); ?>">Prev</a>
     <a href="<?= base_url('grup/'. (($week + 1)<=date("W")-1?($week + 1):"#")); ?>">Next</a>
     <?php 
-    dbg($matkul_data);
+    // dbg($matkul_data);
     ?>
     <table border="1">
         <thead>
             <tr>
-                <th>NIM</th>
-                <th>Nama</th>
+                <th class='t1'>Sync</th>
                 <?php foreach ($matkul_data as $matkul): ?>
-                    <th><?php echo substr($matkul['matkul_singkat'], 0, 3); ?></th>
+                    <th class='sync t2'><?php echo $matkul['sync']; ?></th>
+                <?php endforeach; ?>
+            </tr>
+            <tr>
+                <th class='t1'>Nama</th>
+                <?php foreach ($matkul_data as $matkul): ?>
+                    <th class='t2'><?php echo substr($matkul['matkul_singkat'], 0, 3); ?></th>
                 <?php endforeach; ?>
             </tr>
         </thead>
         <tbody>
             <?php foreach ($rekap_absensi as $rekap): ?>
                 <tr>
-                    <td>&nbsp;</td>
-                    <td><?php echo $rekap['nama']; ?></td>
+                    <td class='t1'><?php echo $rekap['nama']; ?></td>
                     <?php
                     foreach ($matkul_data as $matkul) {
                         $id_matkul = $matkul['id_matkul'];
                         $absen_count = $rekap[$id_matkul];
                         $min_absen = $matkul['min_absen'];
-                        if($absen_count == 'Offline'){
+                        if ($absen_count == 'Offline') {
                             echo "<td class='empty'>" . ($absen_count == 'Offline' ? '-' : $absen_count) . "</td>";
-                        }else{
-                            if($absen_count==0){
-                                $class="class='merah'";
-                            }elseif($absen_count<$min_absen){
-                                $class="class='kurang'";
-                            }else{  
-                                $class="";
+                        } else {
+                            if ($absen_count == 0) {
+                                $class = "merah";
+                            } elseif ($absen_count < $min_absen) {
+                                $class = "kurang";
+                            } else {
+                                $class = "";
                             }
-                            echo "<td $class>" .$absen_count . "</td>";
+                            echo "<td class='t2 $class'>" . $absen_count . "</td>";
                         }
                     }
                     ?>
@@ -80,5 +99,12 @@
             <?php endforeach; ?>
         </tbody>
     </table>
+    <script>
+        // document.addEventListener('DOMContentLoaded', function () {
+        //     const theadHeight = document.querySelector('thead').offsetHeight;
+        //     const tableHeight = window.innerHeight - theadHeight - 50; // Adjust the 50 to your needs
+        //     document.querySelector('tbody').style.maxHeight = tableHeight + 'px';
+        // });
+    </script>
 </body>
 </html>
